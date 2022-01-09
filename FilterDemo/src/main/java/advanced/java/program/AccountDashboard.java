e84cfafdda7418c8ea87720ac98727aff5bda582
OsmitaSamanta <63603002+OsmitaSamanta@users.noreply.github.com>
2022-01-09 22:32:52 +0530

    Add files via upload

---
+++ FilterDemo/src/main/java/advanced/java/program/AccountDashboard.java
@@ -0,0 +1,31 @@
+package advanced.java.program;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AccountDashboard")
+public class AccountDashboard extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public AccountDashboard() {
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		res.getWriter().write("I am in Account Dashboard after passing through LoginFilter");
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		doGet(req, res);
+	}
+
+}
